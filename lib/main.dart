19f8a91da3351c58e3f64dc4548acad2dda4711d
DIGIT Pakistan <info@digit.com.pk>
2021-06-21 15:39:08 +0500

    multidex support

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:housingsociety/models/user.dart';
 import 'package:housingsociety/screens/wrapper.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:housingsociety/services/auth.dart';
 import 'package:provider/provider.dart';
+
 import 'routes.dart';
 
 void main() async {
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
       initialData: null,
       value: AuthService().user,
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData.dark().copyWith(
           primaryColor: Color(0xFF0A0E21),
           scaffoldBackgroundColor: Color(0xFF0A0E21),
